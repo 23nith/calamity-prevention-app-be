3cdfc00574eff6707e36a341313fe30e9e5656e5
23nith <zenithmanuel@gmail.com>
2022-06-05 20:01:52 +0800

    Run db migrate reset

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_05_064431) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jti"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
