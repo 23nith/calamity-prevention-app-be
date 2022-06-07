1abb363d76baf1d5fefcaec65b8e727277428aec
23nith <zenithmanuel@gmail.com>
2022-06-07 19:08:27 +0800

    Add model rspec for area

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+    let!(:area) { Area.new }
+
+    it '1. is not valid without name' do
+      area.address = "Congressional Avenue"
+      area.longitude = 1.5
+      area.latitude = 1.5
+      area.radius = 1.5
+      expect(area).to_not be_valid
+    end
+
+    it '2. is not valid without address' do
+      area.name = "Project 8"
+      area.longitude = 1.5
+      area.latitude = 1.5
+      area.radius = 1.5
+      expect(area).to_not be_valid
+    end
+
+    it '3. is not valid without longitude' do
+      area.name = "Project 8"
+      area.address = "Congressional Avenue"
+      area.latitude = 1.5
+      area.radius = 1.5
+      expect(area).to_not be_valid
+    end
+
+    it '4. is not valid without latitude' do
+      area.name = "Project 8"
+      area.address = "Congressional Avenue"
+      area.longitude = 1.5
+      area.radius = 1.5
+      expect(area).to_not be_valid
+    end
+
+    it '5. is not valid without radius' do
+      area.name = "Project 8"
+      area.address = "Congressional Avenue"
+      area.longitude = 1.5
+      area.latitude = 1.5
+      expect(area).to_not be_valid
+    end
+
+  end
 end
