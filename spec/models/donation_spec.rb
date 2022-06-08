fcfc8e9616dbba1069e1221ebb9a6cad57bd5f6a
23nith <zenithmanuel@gmail.com>
2022-06-08 11:44:07 +0800

    Add model rspec for donation

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Donation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validation" do
+    let!(:donation) {Donation.new}
+
+    it "1. is not valid without user_id" do
+      donation.need_id = 1
+      donation.amount = 100
+      expect(donation).to_not be_valid
+    end
+
+    it "2. is not valid without need_id" do
+      donation.user_id = 1
+      donation.amount = 100
+      expect(donation).to_not be_valid
+    end
+
+    it "3. is not valid without amount" do
+      donation.user_id = 1
+      donation.need_id = 1
+      expect(donation).to_not be_valid
+    end
+
+    it "4. is not valid if amount is not numerical" do
+      donation.user_id = 1
+      donation.need_id = 1
+      donation.amount = "string"
+      expect(donation).to_not be_valid
+    end
+
+  end
 end
