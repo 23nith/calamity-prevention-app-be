e53887358f2bbdf5c0de9aa8714de8089a8b4b3a
23nith <zenithmanuel@gmail.com>
2022-06-08 11:51:53 +0800

    Add model rspec for message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validation" do
+    let!(:message) {Message.new}
+
+    it "1. is not valid without user_id" do 
+      message.contact_person_id = 1
+      message_content = "Help"
+      expect(message).to_not be_valid
+    end
+
+    it "2. is not valid without contact_person_id" do 
+      message.user_id = 1
+      message_content = "Help"
+      expect(message).to_not be_valid
+    end
+
+    it "3. is not valid without message_content" do 
+      message.user_id = 1
+      message.contact_person_id = 1
+      expect(message).to_not be_valid
+    end
+
+    it "4. is not valid if message_content goes beyond 140 characters" do 
+      hundred_and_forty_chars_string = "jtlPG2S2v1sbqSA2WujlEFWSei1WC7Q1J8ILywfX2rQvpb4GdkGuWxYMmVsWrizz0OZbVI8VXv510okvo9qJljF4x7OEqa208b9ZzV3B0ykGbanrRL2Q9xSZ5Dhqe20jOm4Cc2p0IDVi" 
+      message.user_id = 1
+      message.contact_person_id = 1
+      message_content = hundred_and_forty_chars_string + "1"
+      expect(message).to_not be_valid
+    end
+
+  end
 end
