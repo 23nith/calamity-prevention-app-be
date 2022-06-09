ba3dd1f3f912a3ed49feb7b6d1a27c7b464b9636
23nith <zenithmanuel@gmail.com>
2022-06-09 13:31:59 +0800

    Add an expected valid test for calamity model rspec

--- spec/models/calamity_spec.rb
+++ spec/models/calamity_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Calamity, type: :model do
   context "Validations" do
+    let!(:area) {Area.create!(name: "Project 8", address: "Congressional Avenue", longitude: 1.5, latitude: 1.5, radius: 1.5)}
     let!(:calamity) {Calamity.new}
 
     it '1. is not valid without area id' do
@@ -13,6 +14,7 @@ RSpec.describe Calamity, type: :model do
     end
 
     it '2. is not valid without estimated-date-from' do
+      calamity.area_id = area.id
       calamity.estimated_date_to = DateTime.now + 1
       calamity.type = "Flood"
       calamity.description = "Waist deep"
@@ -20,7 +22,7 @@ RSpec.describe Calamity, type: :model do
     end
 
     it '3. is not valid without estimated-date-to' do
-      calamity.area_id = 1
+      calamity.area_id = area.id
       calamity.estimated_date_from = DateTime.now
       calamity.type = "Flood"
       calamity.description = "Waist deep"
@@ -28,7 +30,7 @@ RSpec.describe Calamity, type: :model do
     end
 
     it '4. is not valid if estimated-date-to is an earlier date from estimated-date-from' do
-      calamity.area_id = 1
+      calamity.area_id = area.id
       calamity.estimated_date_from = DateTime.now
       calamity.estimated_date_to = DateTime.now - 1
       calamity.type = "Flood"
@@ -37,7 +39,7 @@ RSpec.describe Calamity, type: :model do
     end
 
     it '5. is not valid without type' do
-      calamity.area_id = 1
+      calamity.area_id = area.id
       calamity.estimated_date_from = DateTime.now
       calamity.estimated_date_to = DateTime.now + 1
       calamity.description = "Waist deep"
@@ -45,7 +47,7 @@ RSpec.describe Calamity, type: :model do
     end
 
     it '6. is not valid without description' do
-      calamity.area_id = 1
+      calamity.area_id = area.id
       calamity.estimated_date_from = DateTime.now
       calamity.estimated_date_to = DateTime.now + 1
       calamity.type = "Flood"
@@ -54,7 +56,7 @@ RSpec.describe Calamity, type: :model do
 
     it '7. is not valid if description goes beyond 140 characters' do
       hundred_and_forty_chars_string = "jtlPG2S2v1sbqSA2WujlEFWSei1WC7Q1J8ILywfX2rQvpb4GdkGuWxYMmVsWrizz0OZbVI8VXv510okvo9qJljF4x7OEqa208b9ZzV3B0ykGbanrRL2Q9xSZ5Dhqe20jOm4Cc2p0IDVi" 
-      calamity.area_id = 1
+      calamity.area_id = area.id
       calamity.estimated_date_from = DateTime.now
       calamity.estimated_date_to = DateTime.now + 1
       calamity.type = "Flood"
@@ -62,16 +64,25 @@ RSpec.describe Calamity, type: :model do
       expect(calamity).to_not be_valid
     end
 
+    it '8. is valid if all fields are present' do
+      calamity.area_id = area.id
+      calamity.estimated_date_from = DateTime.now
+      calamity.estimated_date_to = DateTime.now + 1
+      calamity.type = "Flood"
+      calamity.description = "Waist deep"
+      expect(calamity).to be_valid
+    end
+
   end
 
   context "Association" do
 
-    it "should have many needs" do
+    it "1. should have many needs" do
       t = Calamity.reflect_on_association(:needs)
       expect(t.macro).to eq(:has_many)
     end
 
-    it "should belong to area" do
+    it "2. should belong to area" do
       t = Calamity.reflect_on_association(:area)
       expect(t.macro).to eq(:belongs_to)
     end
