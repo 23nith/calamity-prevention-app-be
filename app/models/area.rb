b13f79ae296a880f0ef40c2c9cf7a5f317197b8a
23nith <zenithmanuel@gmail.com>
2022-06-09 15:09:04 +0800

    Add model relationships

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,7 @@
 class Area < ApplicationRecord
+  has_many :users
+  has_many :calamities
+  has_one :contact_person
   validates :name, presence: true
   validates :address, presence: true, length: { maximum: 140 }
   validates :longitude, presence: true, numericality: true
