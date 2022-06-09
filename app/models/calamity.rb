3bed5f72790de49d31e1a8a5c0027ef611a455d4
23nith <zenithmanuel@gmail.com>
2022-06-09 14:38:08 +0800

    Add validations for calamity model

--- app/models/calamity.rb
+++ app/models/calamity.rb
@@ -1,3 +1,8 @@
 class Calamity < ApplicationRecord
   belongs_to :area
+  validates :area_id, presence: true
+  validates :estimated_date_from, presence: true
+  validates :estimated_date_to, presence: true, comparison: { greater_than: :estimated_date_from, message: "Should be later than date from."  }
+  validates :type, presence: true
+  validates :description, presence: true, length: { maximum: 140 }
 end
