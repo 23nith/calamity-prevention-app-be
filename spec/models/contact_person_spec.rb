eeb23f20a553286bb097d4d118bf5a3e8c2c209f
23nith <zenithmanuel@gmail.com>
2022-06-09 13:34:12 +0800

    Add an expected valid test for contact_person model rspec

--- spec/models/contact_person_spec.rb
+++ spec/models/contact_person_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ContactPerson, type: :model do
-  
+    
   context "Validation" do
+    let!(:area) {Area.create!(name: "Quezon City", address: "Congressional Ave", longitude: 1.5, latitude: 1.5, radius: 1.5)}
+    let!(:user) {User.create!(email: "test@email.com", password: "password", first_name: "firstname", last_name: "lastname", address: "Quezon City", longitude: 1.5, latitude: 1.5, role: "user", area_id: area.id)}
     let!(:contact_person) {ContactPerson.new}
-
+    
     it "1. is not valid without area_id" do
-      contact_person.user_id = 1
+      contact_person.user_id = user.id
       expect(contact_person).to_not be_valid
     end
 
     it "2. is not valid without user_id" do
-      contact_person.area_id = 1
+      contact_person.area_id = user.id
       expect(contact_person).to_not be_valid
+    end
+
+    it "3. is valid if all fields are present" do
+      contact_person.user_id = user.id
+      contact_person.area_id = area.id
+      expect(contact_person).to be_valid
     end
 
   end
