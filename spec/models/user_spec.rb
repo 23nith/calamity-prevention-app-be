8ff3e70a579284c5869753a54c38fb4948fb5809
23nith <zenithmanuel@gmail.com>
2022-06-09 13:52:24 +0800

    Add an expected valid test for user model rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,17 +89,28 @@ RSpec.describe User, type: :model do
       user.role = "user"
       expect(user).to_not be_valid
     end
+    
+    it "9. is valid if all fields are correct" do
+      user.area_id = 1
+      user.first_name = "firstName"
+      user.last_name = "lastName"
+      user.address = "Quezon City"
+      user.longitude = 1.5
+      user.latitude = 1.5
+      user.role = "user"
+      expect(user).to_not be_valid
+    end
 
   end
 
   context "Association" do
 
-    it "should have many donations" do
+    it "1. should have many donations" do
       t = User.reflect_on_association(:donations)
       expect(t.macro).to eq(:has_many)
     end
 
-    it "should belong to area" do
+    it "2. should belong to area" do
       t = User.reflect_on_association(:area)
       expect(t.macro).to eq(:belongs_to)
     end
