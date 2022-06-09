92e1b7a9dab032b7da15197bd239d55561dc0489
23nith <zenithmanuel@gmail.com>
2022-06-09 13:48:19 +0800

    Add an expected valid test for need model rspec

--- spec/models/need_spec.rb
+++ spec/models/need_spec.rb
@@ -57,6 +57,14 @@ RSpec.describe Need, type: :model do
       expect(need).to_not be_valid
     end
 
+    it "8. is valid if all fields are present" do
+      need.calamity_id = 1
+      need.cost = 100
+      need.count = "string"
+      need.description = "Rescue boats"
+      expect(need).to_not be_valid
+    end
+
   end
 
   context "Association" do
