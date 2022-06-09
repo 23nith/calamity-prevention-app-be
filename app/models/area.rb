879e7c1df037b4f0d5cca1d4b68e84e001dec3b3
23nith <zenithmanuel@gmail.com>
2022-06-09 14:36:05 +0800

    Add validations for area model

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,2 +1,7 @@
 class Area < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true, length: { maximum: 140 }
+  validates :longitude, presence: true, numericality: true
+  validates :latitude, presence: true, numericality: true
+  validates :radius, presence: true, numericality: true
 end
