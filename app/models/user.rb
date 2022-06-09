50a6a98133c49b1614fa9a83103e8a460a70c9f1
23nith <zenithmanuel@gmail.com>
2022-06-09 14:58:01 +0800

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :role, presence: true, inclusion: { in: %w(user contact_person admin) }
 end
